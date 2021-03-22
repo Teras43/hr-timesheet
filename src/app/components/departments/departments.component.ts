e75cc6ecad6a336638050d5e67cc3e7023caacb2
Brandon Curtis <Brandcurtis43@gmail.com>
2021-03-22 09:14:34 -0600

    Incorporated an async pipe in the departments.component.ts and the departments.component.html

--- src/app/components/departments/departments.component.ts
+++ src/app/components/departments/departments.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DepartmentsService } from '../../services/departments.service';
 import { Department } from 'src/app/interfaces/department';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-departments',
@@ -9,7 +10,8 @@ import { Router } from '@angular/router';
   styleUrls: ['./departments.component.scss']
 })
 export class DepartmentsComponent implements OnInit {
-  departments: Department[];
+  // departments: Department[];
+  $departments: Observable<Department[]>
 
   constructor(
       private departmentsService: DepartmentsService,
@@ -17,7 +19,10 @@ export class DepartmentsComponent implements OnInit {
     ) { }
 
   ngOnInit(): void {
-      this.departments = this.departmentsService.departments;
+      // this.departmentsService.getDepartments().subscribe(departments => {
+      //   this.departments = departments;
+      // })
+      this.$departments = this.departmentsService.getDepartments();
   }
 
   goToDepartment(departmentId: string): void {
--- src/app/services/departments.service.ts
+++ src/app/services/departments.service.ts
@@ -1,15 +1,25 @@
 import { Injectable } from '@angular/core';
 import { Department } from '../interfaces/department';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DepartmentsService {
-  constructor() { }
+  departmentAPI = 'https://hr-timesheet-test.firebaseio.com/departments.json';
+
+  constructor(
+    private http: HttpClient
+  ) { }
 
   departments: Department[] = [
       {id: '1', name: 'Customer Success'},
       {id: '2', name: 'Sales'},
       {id: '3', name: 'Finance'},
   ];
+
+  getDepartments(): Observable<Department[]> {
+    return this.http.get<Department[]>(this.departmentAPI);
+  }
 }
